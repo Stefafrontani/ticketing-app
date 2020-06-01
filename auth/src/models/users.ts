e80d65800d1019f07b4423a793434e11fc33dd0b
Stefano Frontani <stefano.frontani@globant.com>
2020-06-01 18:45:35 -0300

    Mongoose - Solutions with TS compatibility (2/2) - (140)

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -9,8 +9,16 @@ interface UserAttrs {
 
 // An interface that describes the properties
 // that a user model has
-interface UserModel extends mongoose.Model<any> {
-  build: (attrs: UserAttrs) => any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build: (attrs: UserAttrs) => UserDoc;
+}
+
+// An interface that describes the properties
+// that a user model has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  // updateAt: string; // Add if we would like to access property in user. See (*access to user.updatedAt)
 }
 
 const userSchema = new mongoose.Schema({
@@ -28,12 +36,15 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
-// TO create a new User document we should use all this like - Checked by TS:
-// User.build({
+// const user = User.build({
 //   email: "ads",
 //   password: "asdasd",
 // });
+// Now we can access properties, because user is now of UserDoc type
+// user.email; // good
+// user.password; // good
+// user.updatedAt // We could if interface updated - see (*access to user.updatedAt)
 
 export { User };
