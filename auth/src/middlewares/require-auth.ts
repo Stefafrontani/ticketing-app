1bf93d3940133941610e06bae3f36fb6c3c8df53
Stefano Frontani <stefano.frontani@globant.com>
2020-06-12 17:04:29 -0300

    Client - Ternary signind in or not - Remove error not catch to show not signed in jsx - (216)

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -8,7 +8,8 @@ export const requireAuth = (
 ) => {
   // We assume that the middleware currentUser is run before this middleware
   if (!req.currentUser) {
-    throw new NotAuthorizedError();
+    console.log("req.currentUser");
+    // throw new NotAuthorizedError(); // If i throw and error, it will not be catch and the app will crach
   }
 
   // Assure that if there is a currentUser (user signedin), call the next middleware
