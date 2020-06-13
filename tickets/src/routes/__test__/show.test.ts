11e3023dc7239cec5e6a447bae310acfb1416cdd
Stefano Frontani <stefano.frontani@globant.com>
2020-06-13 19:32:34 -0300

    Tickets - Testing show routes - Unexpected error with show router - Note - (252)

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-  // This test fails unexpectedly - 400 instead of 404
+  // This test fails unexpectedly - 400 instead of 404 - The error is because we are sending an id to the api/tickets/:id that does not match the mongoose id structure
   await request(app).get("/api/tickets/validticketid").send().expect(404);
 });
 
