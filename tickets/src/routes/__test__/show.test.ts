652aa79ddedd94f4759bfc7de7820ae5cd36a62d
Stefano Frontani <stefano.frontani@globant.com>
2020-06-13 19:40:48 -0300

    Tickets - mongoose error related solved - (254)

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
 import request from "supertest";
+import mongoose from "mongoose";
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
   // This test fails unexpectedly - 400 instead of 404 - The error is because we are sending an id to the api/tickets/:id that does not match the mongoose id structure
-  await request(app).get("/api/tickets/validticketid").send().expect(404);
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
