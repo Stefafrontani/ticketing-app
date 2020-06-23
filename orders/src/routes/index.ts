fc57a09c884033f38d9cb10ed9c8483025fd8de6
Stefano Frontani <stefano.frontani@globant.com>
2020-06-23 18:09:14 -0300

    Orders - Fetching a User's Orders - (337)

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
+import { requireAuth } from "@sfticketing/common";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket"); // basically it get the ticket data and put it in that property inside order. order.ticket.whateverMongooseBringsFromTicketDocument
+
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
