5403035ce11940bd31d783fe09029ba29254678a
Stefano Frontani <stefano.frontani@globant.com>
2020-06-24 15:47:22 -0300

    Orders - Fetching Individual Orders - Show route - (339)

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@sfticketing/common";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  /*We could add a middlware to validate orderId mongo structured,*/
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
