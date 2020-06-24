809b92c33384cad9ef9ccfe88403eac61b24f5b7
Stefano Frontani <stefano.frontani@globant.com>
2020-06-24 18:44:16 -0300

    Orders - Testing Event Publishing - (348)

--- orders/src/routes/test/delete.test.ts
+++ orders/src/routes/test/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/tickets";
 import { Order, OrderStatus } from "../../models/orders";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // Create a ticket with Ticket model
@@ -32,4 +33,29 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order cancelled event");
+it("emits an order cancelled event", async () => {
+  // Create a ticket with Ticket model
+  const ticket = Ticket.build({
+    title: "Valid Title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Make a request to build an order with that ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to cancel the order created previously
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
