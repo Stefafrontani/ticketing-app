1d84ae690906d3735d04398d580d35f95d727dca
Stefano Frontani <stefano.frontani@globant.com>
2020-06-24 16:10:40 -0300

    Orders - Cancelling Order - Delete route - (341)

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@sfticketing/common";
+import { Order, OrderStatus } from "../models/orders";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    /*TEchnically we are not deleting, we are updating the record status so it would not be a 204 for delete, nor a method delete on the route butt.. its ok. Not big of a deal */
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
