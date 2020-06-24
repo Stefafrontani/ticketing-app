6125f5a52f2d55965ca75487216d54b95df613b3
Stefano Frontani <stefano.frontani@globant.com>
2020-06-24 16:04:05 -0300

    Orders - Testing Fetching Individual Orders - Show route - (340)

---
+++ orders/src/routes/test/show.test.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/tickets";
+
+it("fetches the order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "Valid Title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to fetch the order - same user that the one used to create ticket in prev step
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch another users order", async () => {
+  // Create a ticket
+  const ticket = Ticket.build({
+    title: "Valid Title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to fetch the order - different user that the one used to create ticket in prev step
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
