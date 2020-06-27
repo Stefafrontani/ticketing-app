ebc9631342a1b6400911691569310bd7bd3aef61
Stefano Frontani <stefano.frontani@globant.com>
2020-06-27 16:16:09 -0300

    Orders & Tickets Event communication - Testing listeners - Skeleton repetead through all listeners tests - (372)

---
+++ orders/src/events/listeners/test/ticket-created-listener.test.ts
@@ -0,0 +1,14 @@
+const setup = async () => {
+  // Create an instance of the listener
+  // Create a fake data event (argument receibed by onMessage function inside listener)
+  // Create a fake message object (argument receibed by onMessage function inside listener)
+};
+it("creates and saves a ticket", () => {
+  // Call the onMessage function with the data object + message object
+  // Write assertions to make sure a ticket was created
+});
+
+it("creates and saves a ticket", () => {
+  // Call the onMessage function with the data object + message object
+  // Write assertions to make sure ack function was called
+});
