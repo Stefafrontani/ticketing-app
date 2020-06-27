5fdbbd73b1371fbcbd4dc078c439ca7601dc8617
Stefano Frontani <stefano.frontani@globant.com>
2020-06-27 13:10:09 -0300

    Orders & Tickets Event communication - Testing Optimistic Concurrency Control - (363)

---
+++ tickets/src/models/test/tickets.test.ts
@@ -0,0 +1,33 @@
+import { Ticket } from "../tickets";
+
+it("implements optimistic concurrency control", async (done) => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "Valid Title",
+    price: 5,
+    userId: "123",
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // Save the first fetched ticket
+  await firstInstance!.save();
+
+  // Save the second fetched ticket - This will have an outdated version number. It was already updated an version increased
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error("Should not reach this point");
+});
