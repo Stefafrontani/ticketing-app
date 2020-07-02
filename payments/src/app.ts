50bef0b1ab2839dc53088e444aaa0c20043a0c40
Stefano Frontani <stefano.frontani@globant.com>
2020-07-02 12:32:54 -0300

    Payments Service - Initial setup: docker, k8s, dependencies, skaffold, etc - (415)

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@sfticketing/common";
+
+const app = express();
+app.set("trust proxy", true); // Express service will be behind nginx proxy so traffic is been proxyed to our app through ingress-nginx. With this line, we tell express to accept (trust) the proxy ingress-nginx anyways
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // not encrypted
+    secure: process.env.NODE_ENV !== "test", // cookies only used when https protection
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,71 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+// This line will make jest to import the file with that same name inside te mock folder we created on this commit, instead of the file of the path itself
+jest.mock("../nats-wrapper");
+
+// MongoMemoryServer
+// This will used to create an isntance of mongo db in memory to run multiple tests at the same time without these tests reaching out to the same instance of mongo
+let mongo: any;
+beforeAll(async () => {
+  jest.clearAllMocks();
+  process.env.JWT_KEY = "asdfasdf";
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    // To mongoose not complain. Not big of a deal
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// Remove all collections before each test
+beforeEach(async () => {
+  jest.clearAllMocks(); // Test was failing because mocks were not being cleared inside that file order-created-listener. When console.log(mock.calls), 3 elements were shown, 3 calls. Only 1 should be been
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// Disconnect from database after all tests run
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(), // whatever in here
+    email: "test@test.com",
+  };
+  // Create the JWT! Use the JWT_KEY key
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Create session object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // Return a string - thats the cookie with the encoded data
+  return [`express:sess=${base64}`]; // The array is because supertest expects an array when including all of our cookies
+};
