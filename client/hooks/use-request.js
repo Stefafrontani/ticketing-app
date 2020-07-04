35d94bb431017b5c32e50381165c7856176efef4
Stefano Frontani <stefano.frontani@globant.com>
2020-07-04 15:24:01 -0300

    Client Side - Navigating to specific order - (451)

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -7,13 +7,14 @@ export default ({ url, method, body, onSuccess }) => {
   const doRequest = async () => {
     try {
       const response = await axios[method](url, body);
-
+      console.log(response);
       if (onSuccess) {
         onSuccess(response.data);
       }
 
       return response.data;
     } catch (error) {
+      console.log(error)
       setErrors(
         <div className="alert alert-danger">
           <h4>Ooops...</h4>
