466eeeff419b9889350e29f2fdbe04b52887b936
Stefano Frontani <stefano.frontani@globant.com>
2020-07-04 14:36:51 -0300

    Client Side - Implement controlled values and sanitize values in new ticket page - (446)

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,41 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value);
+  };
+
   return (
     <div>
       <h1>Create A ticket</h1>
-      <form onSubmit={onSubmit}>
+      <form>
         <div className="form-group">
           <label>Ticket</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" />
+          <input
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
+            onBlur={onBlur}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
@@ -17,4 +43,4 @@ const NewTicket = () => {
   );
 };
 
-export default NewTicket;
+export default NewTicket
